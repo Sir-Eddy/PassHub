249b9f7fe25aee65670699febb8101466a70897a
Elias <150734575+Sir-Eddy@users.noreply.github.com>
2024-12-09 08:49:01 +0100

    delete init

--- target/debug/deps/RataTUI-8f0f79e5db72a6bd.d
+++
@@ -1,5 +0,0 @@
-C:\Users\elias\Documents\GitHub\RataTUI\target\debug\deps\libRataTUI-8f0f79e5db72a6bd.rmeta: src/main.rs
-
-C:\Users\elias\Documents\GitHub\RataTUI\target\debug\deps\RataTUI-8f0f79e5db72a6bd.d: src/main.rs
-
-src/main.rs:
