400565d6d75275f2b678590ca3b2dc54a2679832
Elias <150734575+Sir-Eddy@users.noreply.github.com>
2024-12-09 08:48:53 +0100

    test

---
+++ target/debug/deps/RataTUI-2faaeb78ff629586.d
@@ -0,0 +1,5 @@
+C:\Users\elias\Documents\GitHub\RataTUI\target\debug\deps\libRataTUI-2faaeb78ff629586.rmeta: src/main.rs
+
+C:\Users\elias\Documents\GitHub\RataTUI\target\debug\deps\RataTUI-2faaeb78ff629586.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/RataTUI-8f0f79e5db72a6bd.d
@@ -0,0 +1,5 @@
+C:\Users\elias\Documents\GitHub\RataTUI\target\debug\deps\libRataTUI-8f0f79e5db72a6bd.rmeta: src/main.rs
+
+C:\Users\elias\Documents\GitHub\RataTUI\target\debug\deps\RataTUI-8f0f79e5db72a6bd.d: src/main.rs
+
+src/main.rs:
