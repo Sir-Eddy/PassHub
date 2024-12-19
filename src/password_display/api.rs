aa33977a47aa1a8538b2666e7a3a30e641f32029
DefensivesHolz <gassner.jonas0604@gmail.com>
2024-12-19 10:22:36 +0100

    kompiliert jetzt

--- src/password_display/api.rs
+++ src/password_display/api.rs
@@ -10,11 +10,11 @@ fn get_vault_from_url(backend_url :String, token: String)->Result<String, String
     let vault_url = format!("{}/api/v1/sync/fetch", backend_url);
     
     let client = Client::new();
-    let response = client.get(&vault_url).bearer_auth(token).send()?
-    .map_err(|e| format!("Failed to send request: {}", e));
-
-    match response.status() {
-        StatusCode::OK => respnse.text().map_err(|e| format!("Failed to read response body: {}", e)), //Nochmal überarbeiten
+    let response = client.get(vault_url).bearer_auth(token).send();
+    
+    match response{
+        Ok(res)=> match res.status(){
+        StatusCode::OK => res.text().map_err(|e| format!("Failed to read response body: {}", e)), //Nochmal überarbeiten
         StatusCode::UNAUTHORIZED => {
             debug!("Error: JWT Token is invalid!");
             Err("Unauthorized (401)".to_string())
@@ -24,8 +24,13 @@ fn get_vault_from_url(backend_url :String, token: String)->Result<String, String
             Err("Internal Server Error (500)".to_string())
         }
         _ => {
-            debug!("Unexpected response status: {}", response.status());
-            Err(format!("Unexpected status: {}", response.status()))
+            debug!("Unexpected response status: {}", res.status());
+            Err(format!("Unexpected status: {}", res.status()))
         }
     }
+
+    Err(..) => {debug!("Netzwerkfehler");
+    Err(format!("Network problem")) }
+
+}
 }
